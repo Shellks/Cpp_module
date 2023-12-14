77a3e5127624dd6d0302cf8264f4c7e60d6ae2ce
Alex Carlotti <nagabtc@gmail.com>
2023-12-14 23:09:21 +0100

    debug float '.' before f in progress

--- cpp_06/ex00/src/ScalarConverter.cpp
+++ cpp_06/ex00/src/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: acarlott <acarlott@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 15:01:26 by acarlott          #+#    #+#             */
-/*   Updated: 2023/12/14 20:24:29 by acarlott         ###   ########lyon.fr   */
+/*   Updated: 2023/12/14 23:08:01 by acarlott         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,34 +42,38 @@ ScalarConverter &ScalarConverter::operator=(ScalarConverter const &src)
 */
 void ScalarConverter::convert(std::string const &litteralString)
 {
-	if (litteralString.find_first_not_of("0123456789.-+inaf") == std::string::npos)
+	if (litteralString.find_first_not_of("0123456789.-+inaf") != std::string::npos)
 	{
-		if (litteralString.find_first_not_of("-+ainf") == std::string::npos && litteralString.compare("-inff") &&
-			litteralString.compare("-inf") && litteralString.compare("+inff") && litteralString.compare("+inf") &&
-			litteralString.compare("nanf") && litteralString.compare("nan") && litteralString.compare("-inf"))
-		{
-			if (litteralString.find_first_not_of("-+0123456789") == std::string::npos)
-			{
-				std::cout << RED << "Error: invalid parameters" << std::endl;
-				return;
-			}
-		}
-		if (litteralString.find_first_of(".") != std::string::npos &&
-			(litteralString.find_first_not_of("0123456789.") != std::string::npos || litteralString[0] == '.'))
+		std::cout << RED << "Error: invalid parameters" << std::endl;
+		return;
+	}
+	bool isExist = false;
+	std::string tab[7] = {"-inff", "-inf", "+inff", "+inf", "nanf", "nan", "-inf"};
+	if (litteralString.find_first_of("ain") != std::string::npos)
+	{
+		for (int i = 0; i < 7; i++)
+			if (!litteralString.compare(tab[i]))
+				isExist = true;
+		if (isExist == false)
 		{
 			std::cout << RED << "Error: invalid parameters" << std::endl;
 			return;
 		}
-		char *endptr;
-		double litteralDouble = strtod(litteralString.c_str(), &endptr);
-		std::cout << YELLOW << "Convert" << RESET << " | " << YELLOW << "litteral string " << RESET << "\"" << BLUE << litteralString << RESET << "\"" << std::endl;
-		ConvertToChar(litteralDouble);
-		ConvertToInt(litteralDouble);
-		ConvertToFloat(litteralDouble);
-		ConvertToDouble(litteralDouble);
 	}
-	else
+	//LE IF HASARDEUX RIP
+	if (litteralString.find_first_of("f") != std::string::npos && litteralString.size() != (litteralString.find_first_of("f") + 1) &&
+		(litteralString[litteralString.find_first_of("f") - 1] == '.' || std::isdigit(litteralString[litteralString.find_first_of("f") - 1])))
+	{
 		std::cout << RED << "Error: invalid parameters" << std::endl;
+		return;
+	}
+	char *endptr;
+	double litteralDouble = strtod(litteralString.c_str(), &endptr);
+	std::cout << YELLOW << "Convert" << RESET << " | " << YELLOW << "litteral string " << RESET << "\"" << BLUE << litteralString << RESET << "\"" << std::endl;
+	ConvertToChar(litteralDouble);
+	ConvertToInt(litteralDouble);
+	ConvertToFloat(litteralDouble);
+	ConvertToDouble(litteralDouble);
 }
 
 void ScalarConverter::ConvertToInt(double const litteralDouble)
@@ -98,8 +102,13 @@ void ScalarConverter::ConvertToFloat(double const litteralDouble)
 		return;
 	}
 	int checkDecimal = static_cast<int>(litteralDouble);
-	if (static_cast<double>(checkDecimal) == litteralDouble)
-		std::cout << "        |  -" << MAGENTA << " float  " << RESET << ": " << BLUE << static_cast<float>(litteralDouble) << ".0f" << RESET << std::endl;
+	if (litteralDouble < 999999 && litteralDouble > -999999)
+	{
+		if (static_cast<double>(checkDecimal) == litteralDouble)
+			std::cout << "        |  -" << MAGENTA << " float  " << RESET << ": " << BLUE << static_cast<float>(litteralDouble) << ".0f" << RESET << std::endl;
+		else
+			std::cout << "        |  -" << MAGENTA << " float  " << RESET << ": " << BLUE << static_cast<float>(litteralDouble) << "f" << RESET << std::endl;
+	}
 	else
 		std::cout << "        |  -" << MAGENTA << " float  " << RESET << ": " << BLUE << static_cast<float>(litteralDouble) << "f" << RESET << std::endl;
 }
@@ -120,7 +129,7 @@ void ScalarConverter::ConvertToChar(double const litteralDouble)
 void ScalarConverter::ConvertToDouble(double const litteralDouble)
 {
 	int checkDecimal = static_cast<int>(litteralDouble);
-	if (static_cast<double>(checkDecimal) == litteralDouble)
+	if (static_cast<double>(checkDecimal) == litteralDouble && litteralDouble < 999999 && litteralDouble > -999999)
 		std::cout << "        |  -" << MAGENTA << " Double " << RESET << ": " << BLUE << static_cast<double>(litteralDouble) << ".0" << RESET << std::endl;
 	else
 		std::cout << "        |  -" << MAGENTA << " Double " << RESET << ": " << BLUE << static_cast<double>(litteralDouble) << RESET << std::endl;
